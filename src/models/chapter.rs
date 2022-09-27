268df841ab9ffe128021a414a243492fa0ddeec3
ameyakore <kore.ameya@gmail.com>
2022-09-27 20:30:21 +0530

    added structure to represent entity table

--- src/models/chapter.rs
+++ src/models/chapter.rs
@@ -1,6 +1,16 @@
 use chrono::NaiveDateTime;
 use serde::Serialize;
 
+pub struct ChapterTable {
+    pub chapter_id: String,
+    pub chapter_name: String,
+    pub chapter_number: String,
+    pub updated_at: Option<NaiveDateTime>,
+    pub manga_id: String,
+    pub last_watch_time: i64,
+    pub sequence_number: i32,
+}
+
 #[derive(Serialize, Default, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct MangaChapter {
--- src/models/manga.rs
+++ src/models/manga.rs
@@ -3,6 +3,23 @@ use serde::Serialize;
 
 use super::{author::Author, chapter::MangaChapter, genre::Genre, source::MangaSource};
 
+#[derive(Default, Debug)]
+pub struct MangaTable {
+    pub id: String,
+    pub linked_id: String,
+    pub is_listed: bool,
+    pub cover_url: String,
+    pub url: String,
+    pub last_updated: Option<NaiveDateTime>,
+    pub status: String,
+    pub is_main: String,
+    pub description: String,
+    pub source_id: String,
+    pub last_watch_time: Option<NaiveDateTime>,
+    pub public_id: String,
+    pub is_old: bool
+}
+
 #[derive(Serialize, Default, Debug)]
 pub struct CompleteManga {
     pub main: MainManga,
