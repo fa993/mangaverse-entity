817487c806733c9977b3753b43de6a660a032d7e
ameyakore <kore.ameya@gmail.com>
2022-10-20 14:18:35 +0530

    added equality to genres

--- src/models/genre.rs
+++ src/models/genre.rs
@@ -1,6 +1,6 @@
 use serde::Serialize;
 
-#[derive(Serialize, Default, Debug, Clone)]
+#[derive(Serialize, Default, Debug, Clone, PartialEq, Eq)]
 pub struct Genre {
     pub id: String,
     pub name: String,
